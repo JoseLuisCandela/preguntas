8ada25f22cb326f3c6607575a228deb8b4a59adf
JoseLuisCandela <jlcandela@itpfp.com>
2023-12-01 11:16:53 +0100

    primer commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String preguntas[5]={
+
+            preguntas[0]="¿Cuanto es la mitad de dos mas dos?"
+            preguntas[1]="edificio esmpieza por e y termina por..."
+            preguntas[2]="¿?"
+            preguntas[3]="¿?"
+            preguntas[4]="¿?"
+
+
+
+
+        }
+    }
+}
